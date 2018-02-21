096753419a3452dbb23e8ae717b3ba155c7b2feb
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2018-02-21 14:15:04 +0700

    add AppRouter
    due to make SPA

--- mockup/src/App.js
+++ mockup/src/App.js
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
   render() {
     return (
       <h1>Hello, world</h1>
     );
   }
 }
-
 export default App;
--- mockup/src/index.js
+++ mockup/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppRouter = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(<AppRouter/>, document.getElementById('root'));
 registerServiceWorker();
